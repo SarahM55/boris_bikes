9dfcfb51b03c14e88bed960ebb688048131231f7
Sarah Moffat <sarah.moffat5@gmail.com>
2022-01-25 17:19:03 +0000

    Answered step 9

---
+++ spec/bike_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/bike.rb'
+
+describe Bike do
+    describe "#working?" do
+        it "should respond to the method working?" do
+            bike = Bike.new
+            expect(bike).to respond_to(:working?)
+        end
+    end
+end 
