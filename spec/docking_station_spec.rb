f175a2ad13b15191e74f62fdf891c39d87c94308
Sarah Moffat <96072603+SarahM55@users.noreply.github.com>
2022-01-28 17:15:33 +0000

    Update docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,10 @@
 require_relative '../lib/docking_station.rb'
 
 describe DockingStation do
-
-  let(:bike) {Bike.new}
-
   describe "#release_bike" do
     it "releases a bike" do
-<<<<<<< HEAD
       bike = Bike.new
-      subject.dock(bike)
-=======
       subject.docking_bike(bike)
->>>>>>> refs/remotes/origin/main
       expect(subject.release_bike).to eq bike
     end
 
@@ -21,27 +14,14 @@ describe DockingStation do
   end
   
   describe "#docking bike" do
-<<<<<<< HEAD
     it "should dock bikes" do
       dock_bike = DockingStation.new
-      expect(dock_bike).to respond_to(:dock).with(1).argument
+      expect(dock_bike).to respond_to(:docking_bike).with(1).argument
     end
   
     it "raises an error when the docking station is full when there are 20 bikes" do
-      20.times { subject.dock Bike.new }
-      expect { subject.dock Bike.new }.to raise_error 'Docking station full' 
-=======
-    it "should dock bikes" do      
-      expect(subject).to respond_to(:docking_bike).with(1).argument
-    end
-  
-    it "raises an error when the docking station is full at default capacity" do
-      described_class::DEFAULT_CAPACITY.times do 
-        subject.docking_bike(bike)
-      end
-      
-      expect { subject.docking_bike(bike) }.to raise_error 'Docking Station full' 
->>>>>>> refs/remotes/origin/main
+      20.times { subject.docking_bike Bike.new }
+      expect { subject.docking_bike Bike.new }.to raise_error 'Docking station full' 
     end
   end
 end
