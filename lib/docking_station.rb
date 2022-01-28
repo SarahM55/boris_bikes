0910674b2f0b4386a8cb962a5f368260e9cdd968
camilla <cgreene553@gmail.com>
2022-01-28 16:43:41 +0000

    added dock_full and bikes_available

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,24 +1,32 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bikes
+
+  DEFAULT_CAPACITY = 20
 
     def initialize 
         @bikes = []
+        @capacity = DEFAULT_CAPACITY
     end
 
     def release_bike
-        fail 'No bikes available' unless @bikes
-        @bikes
+        fail 'No bikes available' unless bikes_available?
+        @bikes.pop
     end
 
     def docking_bike(bike)
-      fail 'Docking Station full' if @bikes
-      @bikes = bike
+      fail 'Docking Station full' if dock_full?
+      @bikes << bike
+    end
 
+    private
+
+    def dock_full?
+      @capacity == @bikes.size
     end
 
     def bikes_available?
-        @bikes.empty? ? false : true
+        @bikes.count > 0
     end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/docking_station.rb'
 
 describe DockingStation do
+
+  let(:bike) {Bike.new}
+
   describe "#release_bike" do
     it "releases a bike" do
-      bike = Bike.new
       subject.docking_bike(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -18,18 +20,12 @@ describe DockingStation do
       expect(subject).to respond_to(:docking_bike).with(1).argument
     end
   
-    it "raises an error when the docking station is full" do
-      subject.docking_bike(Bike.new)
-      expect { subject.docking_bike Bike.new }.to raise_error 'Docking Station full' 
+    it "raises an error when the docking station is full at default capacity" do
+      described_class::DEFAULT_CAPACITY.times do 
+        subject.docking_bike(bike)
+      end
+      
+      expect { subject.docking_bike(bike) }.to raise_error 'Docking Station full' 
     end
   end
 end
-
-
-
-#describe DockingStation do
-#  it { expect(DockingStation.new).to respond_to(:release_bike) }
-#  it 'releases working bikes' do
-#    bike = subject.release_bike
-#    expect(bike).to be_working
-#  end
