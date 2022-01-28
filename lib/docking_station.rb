14f097e8bf58ca9df1c2395aaddeba42ee28b5e4
Sarah Moffat <96072603+SarahM55@users.noreply.github.com>
2022-01-28 17:16:28 +0000

    Update docking_station.rb

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,58 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
-<<<<<<< HEAD
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if empty?
-    @bikes.pop
+    @bikes.empty? ? (fail 'No bikes available') : @bikes.pop
   end
 
-  def dock(bike)
-     fail 'Docking station full' if full? 
-     @bikes << bike
+  def docking_bike(bike)
+    @bikes.count >= 20 ? (fail 'Docking station full') : @bikes << bike
   end
-
-  private
-
-  def full?
-    @bikes.count >= 20 ? true : false
-  end
-
-  def empty?
-    @bikes.empty? ? true : false
-  end
-=======
-
-  DEFAULT_CAPACITY = 20
-
-    def initialize 
-        @bikes = []
-        @capacity = DEFAULT_CAPACITY
-    end
-
-    def release_bike
-        fail 'No bikes available' unless bikes_available?
-        @bikes.pop
-    end
-
-    def docking_bike(bike)
-      fail 'Docking Station full' if dock_full?
-      @bikes << bike
-    end
-
-    private
-
-    def dock_full?
-      @capacity == @bikes.size
-    end
-
-    def bikes_available?
-        @bikes.count > 0
-    end
-
->>>>>>> refs/remotes/origin/main
 end
